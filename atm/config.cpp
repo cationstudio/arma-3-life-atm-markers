781fe8592ed3a76dab3c1c400a60151bdd398dcc
Julian Bauer <julian.bauer@cationstudio.com>
2021-12-30 21:00:02 +0100

    Add scripts

---
+++ atm/config.cpp
@@ -0,0 +1,12 @@
+/*
+    File: config.cpp
+    Author: Julian Bauer (julian.bauer@cationstudio.com)
+
+    Description:
+    Config file for atm system. 
+*/
+class Cation_Atm {
+
+    onlyDefault = 0; // mark only default atms
+
+};
---
+++ atm/functions.cpp
@@ -0,0 +1,14 @@
+/*
+    File: functions.cpp
+    Author: Julian Bauer (julian.bauer@cationstudio.com)
+
+    Description:
+    Function config for atm system. 
+*/
+class Cation_Atm {
+    tag = "cat_atm";
+    class functions {
+        file = "cation\atm";
+        class atmMarkers { postInit = 1; };
+    };
+};
